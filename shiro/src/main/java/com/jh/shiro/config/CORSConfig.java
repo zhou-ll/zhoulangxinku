0d68331db23d6824ca1d577d16713cb3e18b70c3
zhoulanglang <1121061245@qq.com>
2020-07-07 09:45:21 +0800

    123

---
+++ shiro/src/main/java/com/jh/shiro/config/CORSConfig.java
@@ -0,0 +1,21 @@
+package com.jh.shiro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author :zll
+ * @PackageName : com.jh.shiro.config
+ * @date :2020/7/7 9:41
+ */
+@Configuration
+public class CORSConfig extends WebMvcConfigurationSupport {
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                .allowCredentials(true).maxAge(3600);
+    }
+}
